8387ec4861b02854046873e5a00c1da14ecda957
Taylor Cooke <taylorcooke@gmail.com>
2020-12-16 20:56:48 -0500

    starting dash project

---
+++ 6-14-20/main.py
@@ -0,0 +1,9 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import requests
+import plotly.graph_objects as go
+
+from dash.dependencies import Input, Output
+
+app = dash.Dash()
