43d5d48bfee87021fc8da1c6a7879da9e0f96347
Taylor Cooke <taylorcooke@gmail.com>
2021-03-22 20:42:45 -0400

    functions written, running

---
+++ 3-15-21/main.py
@@ -0,0 +1,72 @@
+import requests
+import pandas as pd
+import plotly.graph_objects as go
+
+import pandas as pd
+from datetime import datetime
+
+demo = 'd8a1759a992a7461e2cefcb3b2fb5ee4'
+
+def historical_prices(stock):
+	historical_prices = requests.get(f'https://financialmodelingprep.com/api/v3/historical-price-full/{stock}?apikey={demo}').json()
+
+	historical_prices = historical_prices['historical']
+	price_data = {}
+
+	for item in historical_prices:
+		price_data[item['date']] = {}
+		price_data[item['date']]['date'] = item['date']
+		price_data[item['date']]['open']= item['open']
+		price_data[item['date']]['high'] = item['high']
+		price_data[item['date']]['low'] = item['low']
+		price_data[item['date']]['adjClose'] = item['adjClose']
+
+	price_DF = pd.DataFrame.from_dict(price_data)
+	price_DF = price_DF.T	
+	fig = go.Figure(data=[go.Candlestick(x=price_DF['date'],
+                open=price_DF['open'],
+                high=price_DF['high'],
+                low=price_DF['low'],
+                close=price_DF['adjClose'])])
+
+	fig.show()
+
+def income_statement(stock):
+ 	number_qts = input('number_qts').strip()
+ 	IS = requests.get(f'https://financialmodelingprep.com/api/v3/income-statement/{stock}?period=quarter&limit={number_qts}&apikey={demo}').json()
+ 	IS = pd.DataFrame.from_dict(IS)
+ 	print(IS.T)
+ 	save_to_csv = input('save_to_csv? y or n').strip()
+ 	if save_to_csv == 'y':
+ 		IS.to_csv('IS.csv')
+
+def profile(stock):
+	profile = requests.get(f'https://financialmodelingprep.com/api/v3/profile/{stock}?apikey={demo}').json()
+	profile = profile[0]
+	print(profile)
+	
+def balance_sheet(stock):
+	number_qts = input('number_qts?').strip()
+	save_to_csv = input('save_to_csv? y or n').strip()
+	BS = requests.get(f'https://financialmodelingprep.com/api/v3/balance-sheet-statement/{stock}?period=quarter&limit={number_qts}&apikey={demo}').json()
+	BS = pd.DataFrame.from_dict(BS)
+	BS = BS.T
+	if save_to_csv == 'y':
+		BS.to_csv('BS.csv')
+
+	print(BS)
+
+def valuation_dcf(stock):
+	valuation_dcf = requests.get(f'https://financialmodelingprep.com/api/v3/discounted-cash-flow/{stock}?apikey={demo}').json()
+	DCF= valuation_dcf[0]['dcf']
+	stock_price = valuation_dcf[0]['Stock Price']
+	print('Current Stock Price is ' + str(stock_price) + '. Price as per DCF valuation is ',str(DCF))
+
+def dividends(stock):
+	number_of_dividends = input('number_qts?').strip()
+	number_of_dividends = int(number_of_dividends)
+	dividends = requests.get(f'https://financialmodelingprep.com/api/v3/historical-price-full/stock_dividend/{stock}?apikey={demo}').json()
+	dividends = dividends['historical'][0:number_of_dividends]
+
+	for item in dividends:
+		print(item['paymentDate'] + ' :' + ' Dividend was :' + str(item['dividend']))
